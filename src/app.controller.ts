1df42c821fe18c824b307860b52bec8d7917c135
SSumu <sadeepalsumudu@gmail.com>
2026-07-11 20:34:20 +0530

    Some files were updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,9 +8,6 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
   getWelcome() {
     return this.appService.getWelcome();
   }
